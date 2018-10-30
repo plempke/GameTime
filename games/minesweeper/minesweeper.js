2ca439ba3a589c4b10afa0c3610808a01a180819
plempke <31676433+plempke@users.noreply.github.com>
2018-10-29 19:11:28 -0500

    some progress!

--- games/minesweeper/minesweeper.js
+++ games/minesweeper/minesweeper.js
@@ -1,13 +1,6 @@
-<html>
-<body oncontextmenu='return false;' >
-<br><br><br>
-<label id='status'></label>
-
-<script>
-
 //Initial Variable declaration
-const rows = 4;
-const columns = 20;
+const rows = 10;
+const columns = 10;
 let mines, remaining, revealed;
 let status = document.getElementById('status');
 status.addEventListener('click', init)
@@ -54,9 +47,9 @@ function init() {
     }
 
 
-//while our placed amount of items (guess, mine or flag, or program will
-//recognize the amount of open spaces and increment placed to know how many
-//spaces remain to be discovered.
+  //while our placed amount of items (guess, mine or flag, or program will
+  //recognize the amount of open spaces and increment placed to know how many
+  //spaces remain to be discovered.
   let placed = 0;
   do {
     let column = Math.floor(Math.random() * columns);
@@ -67,7 +60,7 @@ function init() {
       placed++;
     }
   } while (placed < mines);
-//checks the row and column to see what spaces are open/taken
+  //checks the row and column to see what spaces are open/taken
   for (let column = 0; column < columns; column++)
     for (let row = 0; row < rows; row++) {
       if (check(row, column) != 'mine') {
@@ -91,8 +84,8 @@ function click(event) {
   let row = Math.floor(id / columns);
   let column = id % columns;
 
-//creates a swith with various cases for our game logic in minesweeper, being that
-//you have the option to palce a flag, question mark, or to unerath a hidden space.
+  //creates a swith with various cases for our game logic in minesweeper, being that
+  //you have the option to palce a flag, question mark, or to unerath a hidden space.
   if (event.which == 3) {
     switch (picture[row][column]) {
       case 'hidden':
@@ -112,12 +105,12 @@ function click(event) {
     }
     event.preventDefault();
   }
-//reports the status of how many mines remain in the game.
+  //reports the status of how many mines remain in the game.
   status.innerHTML = 'Mines remaining: ' + remaining;
 
-//if the item selected is a mine then the system will take noe of that
-//however if a mine is improperly selected then it will track that as well
-//if you select a mine then it will report to you that the game is over
+  //if the item selected is a mine then the system will take noe of that
+  //however if a mine is improperly selected then it will track that as well
+  //if you select a mine then it will report to you that the game is over
   if (event.which == 1 && picture[row][column] != 'flag') {
     if (board[row][column] == 'mine') {
       for (let row = 0; row < rows; row++)
@@ -133,13 +126,13 @@ function click(event) {
     } else
     if (picture[row][column] == 'hidden') reveal(row, column);
   }
-//if you reveal all the rows in columns, minus the mines, then the system will report
-//to you that you won!
+  //if you reveal all the rows in columns, minus the mines, then the system will report
+  //to you that you won!
   if (revealed == rows * columns - mines)
     status.innerHTML = 'YOU WIN!<br><br>Click here to restart';
 }
 //reveal function to determine whether or not a mine has been selected and is used in
-//the previous functions 
+//the previous functions
 function reveal(row, column) {
   tile[row][column].src = board[row][column] + '.png';
   if (board[row][column] != 'mine' && picture[row][column] == 'hidden')
@@ -157,7 +150,3 @@ function reveal(row, column) {
     if (column < (columns - 1) && row > 0 && picture[row - 1][+column + 1] == 'hidden') reveal(row - 1, +column + 1);
   }
 }
-
-</script>
-</body>
-</html>
