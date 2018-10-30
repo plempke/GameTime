c8532da85d77fce53a29a14a129e07a1777cc5e3
Philip Lempke <plempke@luc.edu>
2018-10-30 12:50:20 -0500

    fixed images for home page
    Signed-off-by: Philip Lempke <plempke@luc.edu>

--- games/minesweeper/minesweeper.js
+++ games/minesweeper/minesweeper.js
@@ -12,7 +12,7 @@ let tile = new Array(rows);
 for (let i = 0; i < board.length; i++) {
   board[i] = new Array(columns);
   picture[i] = new Array(columns);
-  tile[i] = new Array(columns)
+  tile[i] = new Array(columns)S
 }
 
 init();
