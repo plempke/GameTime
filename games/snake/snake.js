bc989cad61ba8bd55d69675c0fed4075fd6f3dc3
plempke <31676433+plempke@users.noreply.github.com>
2018-12-01 16:33:20 -0600

    snake object created with FIFO
    Signed-off-by: plempke <31676433+plempke@users.noreply.github.com>

--- games/snake/snake.js
+++ games/snake/snake.js
@@ -48,6 +48,48 @@ grid = {
     get: function(x,y) {
         return this._grid[x][y];
     }
-    
-
 }
+snake = {
+    direction: null,
+    /* number, the direction */
+    last: null,
+    /* Object, pointer to the last element in
+                      the queue */
+    _queue: null,
+    /* Array<number>, data representation*/
+    /**
+     * Clears the queue and sets the start position and direction
+     * 
+     * @param  {number} d start direction
+     * @param  {number} x start x-coordinate
+     * @param  {number} y start y-coordinate
+     */
+    init: function(d, x, y) {
+      this.direction = d;
+      this._queue = [];
+      this.insert(x, y);
+    },
+    /**
+     * Adds an element to the queue
+     * 
+     * @param  {number} x x-coordinate
+     * @param  {number} y y-coordinate
+     */
+    insert: function(x, y) {
+      // unshift prepends an element to an array
+      this._queue.unshift({
+        x: x,
+        y: y
+      });
+      this.last = this._queue[0];
+    },
+    /**
+     * Removes and returns the first element in the queue.
+     * 
+     * @return {Object} the first element
+     */
+    remove: function() {
+      // pop returns the last element of an array
+      return this._queue.pop();
+    }
+  };
